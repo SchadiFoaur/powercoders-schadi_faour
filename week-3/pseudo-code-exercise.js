594a89b24f20d926925f3ee72ecca7041399e19b
schadi faour <schadi.faour@powercoders.org>
2022-04-12 11:38:32 +0200

    week 3

---
+++ week-3/pseudo-code-exercise.js
@@ -0,0 +1,87 @@
+//  Exercise 1
+
+//     Allow the user to input digits, afterwards sort and print them in numerical order.
+
+// start
+// create a variable/empty array to store numbers in it.
+// Allow user to input numbers directly in the variable.
+// check if it is a number.
+// If yes, then add the numbers to the array/ if not, print out "error, try again".
+// compare all the numbers and sort them in ascending order. e.g (1<2<3...).
+// repeat the process.
+// end
+
+// Exercise 2
+
+// Allow the user to input numbers, add them and print the result.
+
+// start
+// create a variable/empty array to store numbers in it.
+// Allow user to input numbers directly in the variable.
+// check if it is a number.
+// If yes, then add the numbers to the array/ if not, print out "error, try again".
+// create variable "sum" with value = 0
+// Add the numbers
+// store the added digits in variable "sum"
+// repeat the process
+// print out the total (sum). eg. (x+y+z...= sum)
+// end
+
+//  Exercise 3
+
+// Allow the user to input numbers, subtract the smaller from the bigger number and print the result.
+
+// start
+// create two empty variables.
+// Allow user to input only two numbers.
+// check if it is a number.
+// If yes, then store each number in one of the two variables./ if not, print out "error, try again".
+// compare the two variables and identify the bigger and smaller variable (var a > var b).
+// create variable "result".
+// Subtract the smaller variable from the bigger variable.
+// store the result in variable "result".
+// print out result.
+// end
+
+// Exercise 4
+
+//     Combine exercise 2 + 3, let the user choose which math operation to do.
+
+// start
+// create two empty variables.
+// Allow user to input only two numbers.
+// check if it is a number.
+// If yes, then store each number in one of the two variables./ if not, print out "error, try again".
+// let user decide if addition or subtraction is needed.
+// create variable "result".
+// if user chooses addition :
+//         var a + var b
+// if user chooses subtraction :
+// compare the two variables and identify the bigger and smaller variable (var a > var b).
+//         var b - var a
+// store result/product in var "result".
+// print out result
+// end
+
+//  Exercise 5
+
+//     Find the day of the year for the given date. For example, January 1st would be 1, and December 31st is 365.
+
+// start
+// create variable "days".
+// create form type date to show a calendar.
+// Allow user to choose a date from the calender.
+// store date in variable "days".
+// if month is January, print out day.
+// if month is February, print out days + 31
+// if month is march, print out days + 31 + 28
+// if month is April, print out days + 31 + 28 + 31
+// if month is May, print out days + 31 + 28 + 31 + 30
+// if month is June, print out days + 31 + 28 + 31 + 30 + 31
+// if month is Juli, print out days + 31 + 28 + 31 + 30 + 31 + 30
+// if month is August, print out days + 31 + 28 + 31 + 30 + 31 + 30 + 31
+// if month is September, print out days + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31
+// if month is October, print out days + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30
+// if month is November, print out days + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31
+// if month is December, print out days + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30
+// end
