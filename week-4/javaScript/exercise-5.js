dfceb917249ddbc9fe0535574be42d062368821b
schadi faour <schadi.faour@powercoders.org>
2022-04-19 21:58:19 +0200

    exercises

---
+++ week-4/javaScript/exercise-5.js
@@ -0,0 +1,17 @@
+// Create a program that prompts for a quote and an author. Display the quotation and author as shown here: [Author] says, "[Quote]" (Replace [Author] and [Quote] with the actual values.
+// constraints : the author must not contain numbers, max 35, normal alphabet. For the quote max 50 char
+// I have a word to say
+
+// start
+// INITIALIZE variable author = ""
+// INITIALIZE variable quote = ""
+// PROMPT for author "What's your name"
+// If author includes number
+//      print out "type a letter, baliz"
+// else if author length is more than 35 char.
+//      print out "max. letters exceeded"
+// PROMPT for quote "Your Quote"
+// If quote length is more than 50 char.
+//      print out "max. letters exceeded"
+// print out author + " says, " + quote + "."
+// end
