94afe337712da8f16d2d36ac05422fd5869455fe
schadi faour <schadi.faour@powercoders.org>
2022-04-21 12:02:42 +0200

    exercises

--- week-4/javaScript/exercise-2.js
+++ week-4/javaScript/exercise-2.js
@@ -1,5 +1,5 @@
-let birth = 2002;
+const birth = 2002;
 let futureDate = 2070;
 let age = futureDate - birth;
-let ageTwo = age + 1;
+let ageTwo = age - 1;
 console.log("I will be " + age + " or " + ageTwo + " in " + futureDate);
