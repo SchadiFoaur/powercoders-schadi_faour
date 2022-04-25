adb4be395f7e304a80979439aee2d62d603d31b1
schadi faour <schadi.faour@powercoders.org>
2022-04-25 15:39:41 +0200

    exercise2

--- week-5/exercise2.js
+++ week-5/exercise2.js
@@ -2,3 +2,13 @@
 // Takes 1 argument, a language code (e.g. "es", "de", "en")
 // Returns "Hello, World" for the given language, for at least 3 languages. It should default to returning English.
 // Call that function for each of the supported languages and log the result to make sure it works
+function hello_world(lang) {
+  if (lang === "en") {
+    console.log("Hello World");
+  } else if (lang === "es") {
+    console.log("Holla Mundo");
+  } else if (lang === "de") {
+    console.log("Hallo Welt");
+  }
+}
+hello_world("es");
